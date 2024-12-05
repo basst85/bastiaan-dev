aedf1232165883e6442214e5e6469fac2e95c9f1
Bastiaan Steinmeier <bastiaan@mecevents.nl>
2024-12-05 21:18:00 +0100

    Add Livewire

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Bastiaan.dev</title>
-        
+
         <!-- Styles / Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
