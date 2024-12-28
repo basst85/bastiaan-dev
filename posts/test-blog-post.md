f695c2ab151651f6b7317d8ef9ca7698e6e73e66
Bastiaan Steinmeier <bastiaan@mecevents.nl>
2024-12-28 23:06:59 +0100

    Update blog page

--- posts/test-blog-post.md
+++ posts/test-blog-post.md
@@ -1,14 +1,23 @@
 ---
-title: Test blog post
+title: Hello, world
 published: true
 publish_date: 2024-12-27 21:00
-updated_date: 2024-12-28 22:00
+updated_date: 2024-12-28 23:00
 author: Bastiaan
-intro: Hello hello
+intro: It's official! I'm finally taking the plunge and launching my personal website.
 tags: test,self
 min_read: 1
 header_image: /images/hello-world.jpg
 ---
-# Hello, world!
+<div class="text-xl">
+<p class="mb-4">
+It's official! I'm finally taking the plunge and launching my personal website.
+</p>
 
-This is a test post
+<p class="mb-4">
+This blog will serve as a platform for me to document my coding journey and share insights and tips. I'm still under construction, but I'm committed to regularly updating this space with new content.
+</p>
+
+
+**Feel free to connect with me on LinkedIn or GitHub**
+</div>
--- resources/views/livewire/show-blogpost.blade.php
+++ resources/views/livewire/show-blogpost.blade.php
@@ -1,20 +1,20 @@
 
 <main class="flex min-h-[calc(100vh-4rem)] flex-col justify-start">
-    <div class="mx-auto max-w-5xl px-4 py-4 md:min-w-[64rem] min-w-full">
+    <div class="mx-auto max-w-5xl px-4 py-4 min-w-full">
         <div class="motion-preset-slide-up-lg mx-auto max-w-5xl">
-            <div class="rounded-lg overflow-hidden md:h-96 h-48">
-                <img src="{{url($blogpost->header_image)}}" alt="{{$blogpost->title}}" class="object-cover object-center hover:scale-105 transition-transform duration-200">
+            <div class="rounded-lg overflow-hidden md:h-96 h-48 w-full">
+                <img src="{{url($blogpost->header_image)}}" alt="{{$blogpost->title}}" class="min-w-full object-cover object-center hover:scale-105 transition-transform duration-200">
             </div>
             <h1 class="text-4xl font-bold text-gray-200 mt-4">
                 {{ $blogpost->title }}
             </h1>
             <p class="flex items-center text-gray-300 mt-4 font-bold text-sm">
                 <x-bi-pencil class="w-4 h-4 inline-block text-gray-200 mr-1" />
-                Written on {{\Carbon\Carbon::parse($blogpost->publish_date)->format('F j, Y')}} by {{$blogpost->author}}
+                Written on {{\Carbon\Carbon::parse($blogpost->publish_date)->format('F j, Y @ H:i')}} by {{$blogpost->author}}
             </p>
             <p class="flex items-center text-gray-300 font-bold text-sm">
                 <x-bi-arrow-repeat class="w-4 h-4 inline-block text-gray-200 mr-1" />
-                Last updated on {{\Carbon\Carbon::parse($blogpost->updated_date)->format('F j, Y')}}
+                Last updated on {{\Carbon\Carbon::parse($blogpost->updated_date)->format('F j, Y @ H:i')}}
             </p>
 
             <p class="border-b-[1px] border-gray-400 w-full mt-4"></p>
